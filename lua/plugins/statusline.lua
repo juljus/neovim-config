9a4755e730de375adba57fdb3b80e65835d2a392
Juljus <juljus@gmail.com>
2025-11-07 19:21:02 +0200

    statusline conf

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,23 +1,38 @@
 return {
     {
         "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional icons for the statusline
         config = function()
             local colors = require("core.colors")
             require("lualine").setup({
                 options = {
                     theme = colors.current_theme,      -- match your colorscheme
-                    section_separators = { left = "", right = "" },
-                    component_separators = { left = "", right = "" },
-                    icons_enabled = true,
+                    section_separators = { left = "", right = "" },
+                    component_separators = { left = ":", right = ":" },
+                    icons_enabled = false,
                 },
                 sections = {
                     lualine_a = { "mode" },
-                    lualine_b = { "branch" },
-                    lualine_c = { "filename" },
-                    lualine_x = { "encoding", "fileformat", "filetype" },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
+                    lualine_b = {
+                        {
+                            "filename",
+                            padding = { left = 1, right = 0 },
+                            file_status = false
+                        },
+                        "progress"
+                    },
+                    lualine_c = { "diagnostics" },
+                    lualine_x = { "lsp_status" },
+                    lualine_y = { "filesize" },
+                    lualine_z = {
+                        {
+                            "branch",
+                            padding = { left = 1, right = 0 }
+                        },
+                        {
+                            "diff",
+                            colored = false
+                        }
+                    },
                 },
             })
         end,
