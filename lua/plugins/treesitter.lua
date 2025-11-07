74a746be33ed9993054c653cfefe57d1a997ad89
Juljus <juljus@gmail.com>
2025-11-07 18:07:26 +0200

    c for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         build = ":TSUpdate",   -- automatically update parsers after install
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css" },
+                ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css", "c" },
                 highlight = {
                     enable = true,  -- enable Treesitter-based syntax highlighting
                     additional_vim_regex_highlighting = false, -- disable default Vim regex highlighting
