f80c9c3e3b60a355a67039d595d193a7479070dc
Juljus <juljus@gmail.com>
2025-11-08 15:04:02 +0200

    keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,17 @@
 -- TELESCOPE
-local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", function()
+    require("telescope.builtin").find_files()
+end, { desc = "Find files" })
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
- 
+-- COMMENT TOGGLING
+vim.keymap.set("n", "<leader>/", function()
+    require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment" })
+
+vim.keymap.set("v", "<leader>/", function()
+    -- Exit visual mode, then toggle comment on selection
+    local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+    vim.api.nvim_feedkeys(esc, "nx", false)
+    require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end, { desc = "Toggle comment" })
+
